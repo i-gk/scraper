0c9ddddadc74ef4a4310a8e234be2fd778988236
Gayan Kumarage <gayank@zone24x7.com>
2020-03-19 19:16:15 -0500

    custom number component added

---
+++ src/components/numbers/number.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NumberTemplate = (props) => (
+    <div style={styles.root} >
+
+        <div style={styles.txtContent}>{props.label}</div>
+        <div style={styles.number}>
+            {props.value}
+        </div>
+        
+    </div>
+);
+
+const styles = {
+    root: {
+        display: 'flex',
+        height: 300, 
+        margin: 'auto'
+    },
+    txtContent: {
+        marginTop: '145px',
+        marginLeft: '20px',
+        fontFamily: 'sans-serif',
+    },
+    number: {
+        flex: 1,
+        marginTop: '15%',
+        marginLeft: '50px',
+        fontFamily: 'sans-serif',
+        fontSize: '180px'
+    }
+}
+
+export default NumberTemplate;
